5e08215f5be5ab3a6a4552d68f3a93e1986848f9
hauretax <hauretax@hotmail.fr>
2019-03-27 10:20:13 +0100

    coucou

--- day02/ex02/loupe.php
+++ day02/ex02/loupe.php
@@ -1,6 +1,11 @@
 #!/usr/bin/php
 <?php
-    $file = fopen($argv[1], 'r+');
-      
+    $file = fopen($argv[1], 'r');
+    while ($ligne = fgets($file))
+    {
+        if (preg_match("/http:\/\/*.*>/", $ligne))
+            echo"XXXXXXXXXXXXXXXXX";
+        echo $ligne;
+    }
     fclose($file);
 ?>
